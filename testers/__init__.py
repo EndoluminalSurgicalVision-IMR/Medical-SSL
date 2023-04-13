5a7f01c6650aa27a739fd84534018387a1be2949
chuyan99 <74494017+chuyan99@users.noreply.github.com>
2023-04-13 23:52:38 +0800

    Update __init__.py

--- testers/__init__.py
+++ testers/__init__.py
@@ -3,3 +3,6 @@ from testers.classification_tester import Classification2CTester
 from testers.seg3d_ROI_tester import Seg3DROITester
 from testers.mg_tester import MGTester
 
+from testers.classification2d_tester import Classification2DTester
+from testers.seg2d_tester import Seg2DROITester
+
