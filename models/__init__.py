399dcc6d496386925f6ba59137c94f5815e9f8b0
chuyan99 <74494017+chuyan99@users.noreply.github.com>
2023-04-13 22:27:22 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,11 +3,12 @@ For some complex methods with multiple network modules.
 """
 
 from models.BYOL import BYOL
-from models.PCRL import PCRLModel3d, PCRLModel3d_AB
+from models.PCRL import PCRLModel3d, PCRLModel2d
+
 
 models_dict= {
     'BYOL': BYOL,
-    #'Pixel_BYOL': PixelBYOL,
+    'PCRL_Model2d': PCRLModel2d,
     'PCRL_Model': PCRLModel3d,
 
 }
@@ -15,10 +16,20 @@ models_dict= {
 def get_models(args, network):
     model_name = args.model
     if model_name == "BYOL":
-        model = BYOL(network, hidden_dim=512, pred_dim=256, m=0.996)
+        if '3d' in args.network:
+            model = BYOL(network, hidden_dim=512, pred_dim=256, m=0.996, encoder_channel=512)
+        else:
+            model = BYOL(network, hidden_dim=512, pred_dim=256, m=0.996, encoder_channel=1024)
+
 
     elif model_name == 'PCRL_Model':
         model = PCRLModel3d(encoder=network[0],
+                            encoder_ema=network[1],
+                            decoder=network[2])
+        
+        
+    elif model_name == 'PCRL_Model2d':
+        model = PCRLModel2d(encoder=network[0],
                             encoder_ema=network[1],
                             decoder=network[2])
 
